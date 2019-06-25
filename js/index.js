925c56c25fee0cfb325e3bbb90c46f579073b86a
Hamidou Diallo <Dio.hamidou@gmail.com>
2019-06-24 21:38:59 -0400

    Added a scroll event

---
+++ js/index.js
@@ -0,0 +1,9 @@
+// const menu = document.querySelector(".main-container");
+// menu.addEventListener("scroll",function(event){
+//     event.target.style.backgroundColor = "#553D67";
+// })
+
+window.addEventListener("scroll", function(ev){
+     document.querySelector(".main-container").style.backgroundColor = "#F64C72";
+
+});
