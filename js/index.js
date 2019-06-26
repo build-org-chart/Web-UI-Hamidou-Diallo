12cf43c3b5abb4fbd3b849523b1bea8a26fbdab4
Hamidou Diallo <Dio.hamidou@gmail.com>
2019-06-26 01:39:39 -0400

    added sticky navbar, still giving me some trouble when scrolling.

--- js/index.js
+++ js/index.js
@@ -4,10 +4,35 @@
 // })
 
 window.addEventListener("scroll", function(ev){
-     document.querySelector(".low-content-3").style.backgroundColor = "#F64C72";
+     document.querySelector(".low-content-2").style.backgroundColor = "#379683";
 
 });
 
 
-TODO: Begin tabs and additional events for the landing page.Begin
+// stick nav bar
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.querySelector("nav");
+
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+
+
+
+
+
+
+// TODO: Begin tabs and additional events for the landing page.Begin
 
