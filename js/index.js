f9e6b92be0aed9ad85bc445bf9b428dad2bc0b7d
Hamidou Diallo <Dio.hamidou@gmail.com>
2019-06-26 19:04:39 -0400

    Drop down menu added

--- js/index.js
+++ js/index.js
@@ -32,29 +32,49 @@ function myFunction() {
 // drop-down menu
 
 // constructor function
-class Dropper{
-    constructor(element){
-        this.element = element;
-        this.bars = this.element.querySelector(".bar1");
-        // console.log("hello");
-        this.links = this.element.querySelector(".link");
-        // console.log("bye");
-        this.bars.addEventListener("click", () => this.toggle());
-    }
+// class Dropper{
+//     constructor(element){
+//         this.element = element;
+//         this.bars = this.element.querySelector(".bar1");
+//         // console.log("hello");
+//         this.links = this.element.querySelector(".link");
+//         // console.log("bye");
+//         this.bars.addEventListener("click", () => this.toggle());
+//     }
 
-    toggle(){
-        this.bars.classList.toggle(".toggle-on");
-        console.log('Hello Tom');
-    }
-}
+//     toggle(){
+//         this.bars.classList.toggle(".toggle-on");
+//         console.log('Hello Tom');
+//     }
+// }
 
 // query selector
-newNav = document.querySelectorAll(".drop-down");
-console.log(newNav);
+// const newNav = document.querySelectorAll(".bar1 .bar2 .bar3");
+// console.log(newNav);
+// newNav.classList.add("mobile-menu");
 
-newNav.forEach(function(items){
-    return new Dropper(items);
-});
+
+const menu = document.querySelectorAll("nav");
+console.log(menu);
+
+// const newNav1 = Array.from(newNav);
+// console.log(newNav1);
+// newNav1.forEach(item => {
+//     item.addEventListener("click", ev => {
+//        ev.classList.toggle("change-1");
+//     });
+// });
+// newNav.addEventListener("click", ev => {
+//     console.log("hello");
+//     // document.querySelectorAll(".bar1").add("change-1");
+// });
+// console.log(newNav);
+
+
+
+// newNav.forEach(function(items){
+//     return new Dropper(items);
+// });
 
 
 
