d3fcf18c5f52533916c70bed8dc84b7dfbee906b
Hamidou Diallo <Dio.hamidou@gmail.com>
2019-06-27 01:59:44 -0400

    replaced place holder images, added events as well

--- js/index.js
+++ js/index.js
@@ -1,10 +1,24 @@
-// const menu = document.querySelector(".main-container");
-// menu.addEventListener("scroll",function(event){
-//     event.target.style.backgroundColor = "#553D67";
-// })
+// Event Listeners
+const btnDesk = document.querySelectorAll(".btn-1");
+console.log(btnDesk);
 
+
+btnDesk.forEach(function(item){
+    item.addEventListener("click", ev =>{
+        ev.target.style.backgroundColor = "white";
+        ev.target.style.color = "#F64C72";
+        
+        setTimeout(function(){
+            ev.target.backgroundColor = " ";
+            ev.target.color = " ";
+        },200);
+    },false);
+});
+
+
+// Components
 window.addEventListener("scroll", function(ev){
-     document.querySelector(".low-content-2").style.backgroundColor = "#379683";
+    document.querySelector(".low-content-2").style.backgroundColor = "#379683";
 
 });
 
@@ -22,28 +36,12 @@ var sticky = navbar.offsetTop;
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
-  if (window.pageYOffset > sticky) {
-    navbar.classList.add("sticky");
-  } else {
-    navbar.classList.remove("sticky");
-  }
+ if (window.pageYOffset > sticky) {
+   navbar.classList.add("sticky");
+ } else {
+   navbar.classList.remove("sticky");
+ }
 }
-
-// class Menu{
-//     constructor(element){
-//         this.element = element;
-//         this.bars = this.element.queryselector(".bar1 .bar3");
-//         console.log(this.bars);
-//         this.newMenu = this.element.queryselector(".nav-end");
-//         console.log(newMenu);
-//         this.bars.addEventListener("click", () => this.toggle());
-//     }
-    
-//     toggle(){
-//         console.log("hello");
-//         this.newMenu.classList.add(".toggle-off");
-//     }
-// }
 
 const navSlide = () =>{
     const bars = document.querySelector(".drop-down-bar");
@@ -66,58 +64,7 @@ navSlide();
 
 
 
-// const menus = document.querySelector(".drop-down-bar");
-// console.log(menus);
 
-// menus.forEach(function(items){
-//     return new Menu(items);
-// });
-
-// drop-down menu
-
-// constructor function
-// class Dropper{
-//     constructor(element){
-//         this.element = element;
-//         this.bars = this.element.querySelector(".bar1");
-//         // console.log("hello");
-//         this.links = this.element.querySelector(".link");
-//         // console.log("bye");
-//         this.bars.addEventListener("click", () => this.toggle());
-//     }
-
-//     toggle(){
-//         this.bars.classList.toggle(".toggle-on");
-//         console.log('Hello Tom');
-//     }
-// }
-
-// query selector
-// const newNav = document.querySelectorAll(".bar1 .bar2 .bar3");
-// console.log(newNav);
-// newNav.classList.add("mobile-menu");
-
-
-
-
-// const newNav1 = Array.from(newNav);
-// console.log(newNav1);
-// newNav1.forEach(item => {
-//     item.addEventListener("click", ev => {
-//        ev.classList.toggle("change-1");
-//     });
-// });
-// newNav.addEventListener("click", ev => {
-//     console.log("hello");
-//     // document.querySelectorAll(".bar1").add("change-1");
-// });
-// console.log(newNav);
-
-
-
-// newNav.forEach(function(items){
-//     return new Dropper(items);
-// });
 
 
 
